c8df794121d5a7f9f9d423404ed957ec77b84ba1
Machi <mshimme@ncsu.edu>
2018-11-27 14:40:01 -0500

    program for parameters

---
+++ SimulationStudy/CourseGenerator/src/ActionType.java
@@ -0,0 +1,73 @@
+/**
+ * Created: Jan 19, 2018 8:57:16 PM
+ * @author mazda
+ *
+ */
+
+/**
+ * @author mazda
+ *
+ */
+
+
+
+public enum ActionType {
+
+		// Video
+		VIDEO ("Video"),
+		// PageView
+//		PAGE ("Page"),
+		// Assessment quiz
+		QUIZ ("Quiz"),
+		//hint
+		DLINK ("DLink");
+
+		// - - - - - - - - - -
+		// Constructor
+		// - - - - - - - - - -
+
+		private String label;
+
+		ActionType (String label) {
+			this.label = label;
+		}
+
+		// - - - - - - - - - -
+		// Getter
+		// - - - - - - - - - -
+
+		/**
+		 *  Returns a prefix for the action
+		 */
+		String getLabel() {
+			return this.label;
+		}
+
+		public static ActionType getTypeByValue(String label)
+	    {
+	        for(ActionType actionType : values())
+	        {
+	            if(actionType.getLabel().equals(label))
+	            {
+	                return actionType;
+	            }
+	        }
+	        throw new IllegalArgumentException("undefined : " + label);
+	    }
+
+
+
+	}
+
+//	/**
+//	 *  Returns a actionType whose label is label.
+//	 */
+//	public ActionType getType(String label) {
+//	    ActionType[] types = ActionType.values();
+//	    for (ActionType actiontype : types) {
+//	        if (actiontype.getLabel() == label) {
+//	            return actiontype;
+//	        }
+//	    }
+//	    return null;
+//	}
