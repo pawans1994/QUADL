62542e9cca01e363bc360ebdb03376deabf1bca4
psmendir <psmendir@ncsu.edu>
2020-02-19 14:38:53 -0500

    Update parseXML.py

--- QUADL/OLI/pre-process/parseXML.py
+++ QUADL/OLI/pre-process/parseXML.py
@@ -23,76 +23,83 @@ with open('/Users/pawan/Documents/newfile.csv', 'w', newline='') as r:
         #if "assessment" in path2:
             #print(path2)
        # if "assessment" in path:
-    try:
-        for filename in glob.glob(os.path.join(path, "*.xml")):
-         #print(filename)
-            with open(filename) as open_file:
-               # print(filename)
-                tree = ET.parse(open_file)
-                root=tree.getroot()
-                for elem in tree.iter():
-                    i = 0
-                    if elem.tag=="multiple_choice":
+try:
+    option = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
+    for filename in glob.glob(os.path.join(path, "*.xml")):
+     #print(filename)
+        with open(filename) as open_file:
+           # print(filename)
+            tree = ET.parse(open_file)
+            root=tree.getroot()
+            for elem in tree.iter():
+                i = 0
+                if elem.tag=="multiple_choice":
 
-                        mc = []
-                        mc.insert(0, ' '.join(path.split("/")[7]))
-                        mc.insert(1, ' '.join(path.split("/")[8]))
-                        for q in elem.iter():
-                            ques = ""
-                            choices=""
-                            correct_ans=""
+                    mc = []
+                    mc.insert(0, ''.join(path.split("/")[7]))
+                    mc.insert(1, ''.join(path.split("/")[8]))
+                    for q in elem.iter():
+                        ques = ""
+                        choices=""
+                        correct_ans=""
 
-                            if q.tag=="body":
-                                #j=0
+                        if q.tag=="body":
+                            #j=0
 
-                                for e in q.iter():
-                                    if(e.text is not None and e.text!="\\*"):
-                                        ques=ques+e.text
-                                if ques and ques not in mc:
-                                     mc.insert(2, ' '.join(ques.split()))
-                                   # print(f'{j}:{e.text}')
-                                    #j+=1
-                                #print(ques.strip())
+                            for e in q.iter():
+                                if(e.text is not None and e.text!="\\*"):
+                                    ques=ques+e.text
+                            if ques and ques not in mc:
+                                 mc.insert(2, ' '.join(ques.split()))
+                               # print(f'{j}:{e.text}')
                                 #j+=1
-                            elif q.tag=="input":
-                                i=0
-                                for e in q.iter():
-                                    if(e.text is not None):
-                                        choices=choices+str(i)+": "+e.text+"\n"
-                                        i+=1
-                                if choices and choices not in mc:
-                                    mc.insert(3, ' '.join(choices.split()))
-                                #print(choices[2:].strip())
-                            elif q.tag=="response":
-                                #dic={}
-                                #print(q.attrib)
-                                if(int(q.attrib['score'])>0):
-                                    correct_ans=q.attrib['match']
-                            if correct_ans and correct_ans not in mc:
-                                    mc.insert(4, ' '.join(correct_ans.split()))
-                            #print(correct_ans.strip())
-                            #m=' '.join(path.split('/')[8])
-                            #data=[ques,choices,correct_ans]
-                       # print(f'For {i}')
+                            #print(ques.strip())
+                            #j+=1
+                        elif q.tag=="input":
+                            i=0
+                            ch = []
+                            for e in q.iter():
+                                if(e.text is not None):
+                                    choices=choices+str(i)+": "+e.text+"\n"
+                                    ch.append(e.text)
+                                    i+=1
+                            if choices and choices not in mc:
+                                mc.insert(3, ' '.join(choices.split()))
+                            #print(choices[2:].strip())
+                        elif q.tag=="response":
+                            answer_text = ""
+                            if (int(q.attrib['score']) > 0):
+                                correct_ans = q.attrib['match']
+                                answer_text = answer_text + ch[option[str(correct_ans)]]
+                                #print(f'Answer is{answer_text}')
+                            if answer_text is not None and answer_text not in mc:
+                                #print(answer_text)
+                                mc.insert(4, ' '.join(answer_text.split()))
+                        #print(correct_ans.strip())
+                        #m=' '.join(path.split('/')[8])
+                        #data=[ques,choices,correct_ans]
+                   # print(f'For {i}')
+                    for i in range (0,len(mc)-1):
+                        if not mc[i]:
+                            mc.remove(mc[i])
+                    print(mc[4])
+                   # i+=1
+                        #co=correct_ans
+                    with open('/Users/pawan/Documents/newfile.csv', "a") as fp:
+                        wr = csv.writer(fp, dialect='excel')
+                        wr.writerow(mc)
 
-                        print(mc)
-                       # i+=1
-                            #co=correct_ans
-                        with open('/Users/pawan/Documents/newfile.csv', "a") as fp:
-                            wr = csv.writer(fp, dialect='excel')
-                            wr.writerow(mc)
 
-
-                                    #if(len(dic)!=0):
-                                      #  print(dic['match'])
-                                    #if len(res.attrib)>0 and int(res.attrib["score"]) > 0:
-                                      #  print(res.attrib["match"])
-                       #i=0
-                        #for e in elem.iter():
-                          #  print(f' {i} : {e.text}')
-                           # i=i+1'''
-    except ET.ParseError:
-        print("Exception occured for"+filename)
+                                #if(len(dic)!=0):
+                                  #  print(dic['match'])
+                                #if len(res.attrib)>0 and int(res.attrib["score"]) > 0:
+                                  #  print(res.attrib["match"])
+                   #i=0
+                    #for e in elem.iter():
+                      #  print(f' {i} : {e.text}')
+                       # i=i+1'''
+except ET.ParseError:
+    print("Exception occured for"+filename)
 
 #for i in range(0,len(ques)):
     #print(ques[i])
