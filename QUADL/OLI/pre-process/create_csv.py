bce897ff562cb7c3055334b36a9bccdabb0ea24c
psmendir <psmendir@ncsu.edu>
2020-02-19 16:59:03 -0500

    Update create_csv.py

--- QUADL/OLI/pre-process/create_csv.py
+++ QUADL/OLI/pre-process/create_csv.py
@@ -1,6 +1,9 @@
 import csv
 
-with open('/Users/pawan/Documents/newfile.csv', 'a') as f:
+with open('/Users/pawan/Documents/multiple_choice.csv', 'a') as f:
     # print(','.join(['Unit', 'Module', 'Question','Choices','Correct Answer']), file = f)
     writer = csv.writer(f)
     writer.writerow(['Unit', 'Module', 'Question', 'Choices', 'Correct Answer'])
+with open('/Users/pawan/Documents/learning_objectives.csv', 'w', newline='') as r:
+    writer = csv.writer(r)
+    writer.writerow(['Unit','Module','Learning Objectives'])
