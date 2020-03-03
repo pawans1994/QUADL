55c2c0c6338b030bbbd9eb3bef5b6d4cb8e2bb99
psmendir <psmendir@ncsu.edu>
2020-03-03 11:41:57 -0500

    Update parse_xml.py

--- QUADL/OLI/pre-process/parse_xml.py
+++ QUADL/OLI/pre-process/parse_xml.py
@@ -9,13 +9,18 @@ def parse_csv(filename):
         option={'A':1,'B':2,'C':3,'D':4,'E':5}
         with open(filename) as open_file:
             tree = ET.parse(open_file)
+            root=tree.getroot()
             for elem in tree.iter():
+                for t in root.findall('.//'):
+                    if t.tag=='fill_in_the_blank':
+                        type_ques='f_i_b'
+                    elif t.tag == 'short_answer':
+                        type_ques = 's_a'
+                    elif t.tag == 'multiple_choice':
+                        type_ques = 'm_c'
                 if elem.tag=="workbook_page":
-                    #print(p.text)
                     unit=''.join(filename.split("/")[8])
                     module = ''.join(filename.split("/")[9])
-                   # print(unit)
-                    #print(module)
                     for i in elem.iter():
                         if i.tag=="body":
                             it = 0
@@ -34,12 +39,10 @@ def parse_csv(filename):
                                         table_row.pop()
                             stri = ""
                             for tags in i.findall('.//'):
-                                # stri=""
                                 i = 0
                                 if tags.text and tags.tail:
                                     stri = stri + tags.text + tags.tail
                             text = re.sub('\s+', ' ', stri)
-                            # new_text=re.findall(r'(?<!\d)\.(?!\d) | \.(?!\d)',text)
                             new_text = nltk.sent_tokenize(text)
                             with open('/Users/pawan/Documents/paragraph.csv', 'a') as fp:
                                 wr = csv.writer(fp, dialect='excel')
@@ -51,8 +54,86 @@ def parse_csv(filename):
                                     wr.writerow(text_list)
                                     text_list.pop()
                             break
-                                    #it+=1
-                           # new_text=[]
+                if elem.tag=="question":
+
+                    mc = []
+                    mc.insert(0, ''.join(filename.split("/")[8]))
+                    mc.insert(1, ''.join(filename.split("/")[9]))
+                    for q in elem.iter():
+                        ques = ""
+                        choices=""
+                        if q.tag=="body":
+                            if q.text is not None:
+                                ques=ques+q.text
+                            for e in q.findall('.//'):
+                                if e.text is not None:
+                                    ques=ques+e.text
+                            if ques and ques not in mc:
+                                 mc.insert(2, ' '.join(ques.split()))
+                        elif q.tag=="multiple_choice":
+                            i=0
+                            ch = {}
+                            for e in q.iter():
+                                if(e.text is not None):
+                                    choices=choices+str(i)+": "+e.text+"\n"
+                                    if 'value' in e.attrib:
+                                        ch[e.attrib['value']] = ' '.join(e.text.split())
+                                    i+=1
+                            if choices and choices not in mc:
+                                mc.insert(3, ' '.join(choices.split()))
+                        elif q.tag=="fill_in_the_blank":
+                            option = q.attrib['id']
+                            for e in q.iter():
+                                if e.text is not None:
+                                    choices=choices+e.text+"\n"
+                            choices = option+": "+choices
+                            if choices and choices not in mc:
+                                if len(mc) == 4:
+                                    mc[3] = mc[3] + choices
+                                else:
+                                    mc.insert(3, choices)
+                        elif q.tag=="response":
+                            answer_text = ""
+                            if type_ques == 'm_c':
+                                if 'score' in q.attrib:
+                                    if (int(q.attrib['score']) > 0):
+                                        correct_ans = q.attrib['match']
+                                        correct_choice=correct_ans.split(',')
+                                        for cc in correct_choice:
+                                            if cc in ch:
+                                                answer_text = answer_text + ch[cc]+"\n"
+                                if answer_text is not None and answer_text not in mc:
+                                    mc.insert(4, ' '.join(answer_text.split()))
+                            elif type_ques == 'f_i_b':
+                                if 'score' in q.attrib:
+                                    option=q.attrib['input']
+                                    answer_text=answer_text+q.attrib['name']+"\n"
+                                    answer_text=option+": "+answer_text
+                                    if len(mc)==5:
+                                        mc[4]=mc[4]+answer_text
+                                    else:
+                                        mc.insert(4, answer_text)
+                        elif q.tag =='explanation':
+                            if type_ques =='s_a':
+                                for sub_tag in q.iter():
+                                    if sub_tag.text is not None and sub_tag.tail is not None:
+                                        answer_text=answer_text+sub_tag.text+sub_tag.tail
+                            mc.insert(4, ' '.join(answer_text.split()))
+                    for i in range (0,len(mc)-1):
+                        if not mc[i]:
+                            mc.remove(mc[i])
+                    if type_ques=='m_c':
+                        with open('/Users/pawan/Documents/multiple_choice.csv', "a") as fp:
+                            wr = csv.writer(fp, dialect='excel')
+                            wr.writerow(mc)
+                    elif type_ques == 'f_i_b':
+                        with open('/Users/pawan/Documents/fill_in_the_blanks.csv', "a") as fp:
+                            wr = csv.writer(fp, dialect='excel')
+                            wr.writerow(mc)
+                    elif type_ques=='s_a':
+                        with open('/Users/pawan/Documents/short_answer.csv', "a") as fp:
+                            wr = csv.writer(fp, dialect='excel')
+                            wr.writerow(mc)
                 if elem.tag=="multiple_choice":
                     mc = []
                     mc.insert(0, ''.join(filename.split("/")[8]))
@@ -60,7 +141,6 @@ def parse_csv(filename):
                     for q in elem.iter():
                         ques = ""
                         choices=""
-                        correct_ans=""
 
                         if q.tag=="body":
                             for e in q.iter():
@@ -88,7 +168,6 @@ def parse_csv(filename):
                     for i in range(0,len(mc)-1):
                         if not mc[i]:
                             mc.remove(mc[i])
-                    #print(mc)
                     if len(mc)>2:
                         with open("/Users/pawan/Documents/multiple_choice.csv", "a", newline='\n') as fp:
                              mc = [val.replace(',',';') for val in mc]
