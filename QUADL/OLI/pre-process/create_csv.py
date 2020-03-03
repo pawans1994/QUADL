2a16e9ecf2c95a1b5083a13aa2dad12d88180f74
psmendir <psmendir@ncsu.edu>
2020-03-03 11:43:45 -0500

    Update create_csv.py

--- QUADL/OLI/pre-process/create_csv.py
+++ QUADL/OLI/pre-process/create_csv.py
@@ -7,3 +7,12 @@ with open('/Users/pawan/Documents/multiple_choice.csv', 'a') as f:
 with open('/Users/pawan/Documents/learning_objectives.csv', 'w', newline='') as r:
     writer = csv.writer(r)
     writer.writerow(['Unit','Module','Learning Objectives'])
+with open('/Users/pawan/Documents/paragraph.csv', 'w', newline='') as r:
+    writer = csv.writer(r)
+    writer.writerow(['Unit','Module','Sentences'])
+with open('/Users/pawan/Documents/fill_in_the_blanks.csv', 'w', newline='') as r:
+    writer = csv.writer(r)
+    writer.writerow(['Unit', 'Module', 'Question', 'Choices', 'Correct Answer'])
+with open('/Users/pawan/Documents/short_answer.csv', 'w', newline='') as r:
+    writer = csv.writer(r)
+    writer.writerow(['Unit', 'Module', 'Question', 'Explanation'])
